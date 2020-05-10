a59c33fd7e73e18c5555ff5b800d35fadbe70d32
Griffin Beels <griffin_beels@brown.edu>
2020-05-09 23:13:36 -1000

    filtering works in real time

--- py/filter.py
+++ py/filter.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import cv2 as cv
-from skimage import io
 import argparse
 import math
 import numpy as np
@@ -12,14 +11,31 @@ from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras.models import load_model
 import os
+import enum
+
+# Emotion enums
+class Emotion(enum.Enum): 
+    angry = 0
+    disgusted = 1
+    fearful = 2
+    happy = 3
+    neutral = 4
+    sad = 5
+    surprised = 6
 
 # This file is run from the /py directory -- therefore, we go up a directory
 TRAINING_DIRECTORY = "data/train"
 TESTING_DIRECTORY = "data/test"
 MODEL_DATA_FILE_PATH = "data/model.h5"
 
-# Maps numbers [0, 6] to each emotion, for use in processing the model classification 
-EMOTIONS = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+# Maps numbers [0, 6] to each emotion, for use in processing the model classification; uses Emotion enum
+EMOTIONS = {Emotion.angry: "Angry",
+            Emotion.disgusted: "Disgusted",
+            Emotion.fearful: "Fearful", 
+            Emotion.happy: "Happy", 
+            Emotion.neutral: "Neutral", 
+            Emotion.sad: "Sad", 
+            Emotion.surprised: "Surprised"}
 
 # Research paper (in root directory) calls for all input to be 48x48
 INPUT_DIM = 48
@@ -39,22 +55,15 @@ def detect_and_display(frame, face_cascade, eyes_cascade, model):
     faces = face_cascade.detectMultiScale(frame_gray)
     # Set some face resc cut off for no weird jank.
     face_radius = 100
-    # Classify emotion (single face TODO expand to all faces)
-    # and apply filter to frame
-    # if faces[0] != None:
-    #     emotion = classifyEmotion(faces[0])
-    #     cv.imshow('emotion-based-filter', applyFilterToFrame)
-    # s_img = cv.imread("img/dog_emote_raw.jpg")
-    # s_img = cv.resize(s_img, (320, 240))
     
     # print(s_img.shape)
     eye_list = np.zeros((2,2), dtype=int)
     face = False
-    if len(faces) > 0:
+    if len(faces) > 0: # at least one face is detected, use the first one! TODO: add support for more than one face in frame 
+        (x, y, w, h) = faces[0]
         emotion = classify_emotion(frame_gray, faces[0], model)
         print(EMOTIONS[emotion])
-
-    for (x,y,w,h) in faces:
+    # for (x,y,w,h) in faces:
         # print(frame.shape)
         face = True
         center = (x + w//2, y + h//2)
@@ -77,33 +86,57 @@ def detect_and_display(frame, face_cascade, eyes_cascade, model):
         #     radius = int(round((w2 + h2)*0.25))
         #     frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
 
-    if face and (x_offset2 < frame.shape[0]):
-        left_ear_img = cv.imread("img/ear_happy_left.png", -1)
-        right_ear_img = cv.imread("img/ear_happy_right.png", -1)
-        nose = cv.imread("img/nose.png", -1)
-        y1, y2 = eye_list[0][1]-left_ear_img.shape[0], eye_list[0][1]
-        x1, x2 = eye_list[0][0]-left_ear_img.shape[1], eye_list[0][0] 
-        y11, y21 = eye_list[1][1]-right_ear_img.shape[0], eye_list[1][1]
-        x11, x21 = eye_list[1][0], eye_list[1][0]+right_ear_img.shape[1]
-        y12, y22 = y_offset2-math.ceil(nose.shape[0]/2), y_offset2+math.floor(nose.shape[0]/2)
-        x12, x22 = x_offset2-math.ceil(nose.shape[1]/2), x_offset2+math.floor(nose.shape[1]/2)
-        alpha_s = left_ear_img[:, :, 3] / 255.0
-        alpha_s1 = right_ear_img[:, :, 3] / 255.0
-        alpha_s2 = nose[:, :, 3] / 255.0
-        alpha_l = 1.0 - alpha_s
-        alpha_l1 = 1.0 - alpha_s1
-        alpha_l2 = 1.0 - alpha_s2
-        for c in range(0, 3):
-            frame[y1:y2, x1:x2, c] = (alpha_s * left_ear_img[:, :, c] + alpha_l * frame[y1:y2, x1:x2, c])
-            frame[y11:y21, x11:x21, c] =  (alpha_s1 * right_ear_img[:, :, c] + alpha_l1 * frame[y11:y21, x11:x21, c])
-            frame[y12:y22, x12:x22, c] =  (alpha_s2 * nose[:, :, c] + alpha_l2 * frame[y12:y22, x12:x22, c])
+        if face and (x_offset2 < frame.shape[0]):
+            # Determine which ear combo should be used 
+            (left_ear_img, right_ear_img) = choose_filter(emotion)
+
+            nose = cv.imread("img/nose.png", -1)
+            y1, y2 = eye_list[0][1]-left_ear_img.shape[0], eye_list[0][1] # TODO: error here?
+            x1, x2 = eye_list[0][0]-left_ear_img.shape[1], eye_list[0][0] # error here?
+            y11, y21 = eye_list[1][1]-right_ear_img.shape[0], eye_list[1][1]
+            x11, x21 = eye_list[1][0], eye_list[1][0]+right_ear_img.shape[1]
+            y12, y22 = y_offset2-math.ceil(nose.shape[0]/2), y_offset2+math.floor(nose.shape[0]/2)
+            x12, x22 = x_offset2-math.ceil(nose.shape[1]/2), x_offset2+math.floor(nose.shape[1]/2)
+            alpha_s = left_ear_img[:, :, 3] / 255.0
+            alpha_s1 = right_ear_img[:, :, 3] / 255.0
+            alpha_s2 = nose[:, :, 3] / 255.0
+            alpha_l = 1.0 - alpha_s
+            alpha_l1 = 1.0 - alpha_s1
+            alpha_l2 = 1.0 - alpha_s2
+            for c in range(0, 3):
+                frame[y1:y2, x1:x2, c] = (alpha_s * left_ear_img[:, :, c]) + (alpha_l * frame[y1:y2, x1:x2, c])
+                frame[y11:y21, x11:x21, c] =  (alpha_s1 * right_ear_img[:, :, c] + alpha_l1 * frame[y11:y21, x11:x21, c])
+                frame[y12:y22, x12:x22, c] =  (alpha_s2 * nose[:, :, c] + alpha_l2 * frame[y12:y22, x12:x22, c])
 
     cv.imshow('Capture - Face detection', frame)
 
+def choose_filter(emotion):
+    """
+    Chooses the correct filter to place over the user based on their perceived emotion.
+    The mappings are as follows:
+        HAPPY - Happy, Surprised
+        SAD - Sad, Angry, Disgusted
+        NEUTRAL - Neutral, Fearful 
+    Fearful is included under the neutral filter because it seems that our dataset overfit for Fearful -- 
+    for this reason, misclassifications will be sent to neutral if they are fearful.
+    """
+    # TODO: Change the scaling of each image (as each is slightly differently sized & oriented)
+    if emotion == Emotion.happy or emotion == Emotion.surprised:
+        left_ear_img = cv.imread("img/ear_happy_left.png", -1)
+        right_ear_img = cv.imread("img/ear_happy_right.png", -1)
+    elif emotion == Emotion.sad or emotion == Emotion.angry or emotion == Emotion.disgusted:
+        left_ear_img = cv.imread("img/ear_sad_left.png", -1)
+        right_ear_img = cv.imread("img/ear_sad_right.png", -1)
+    else: # Fearful or Neutral 
+        left_ear_img = cv.imread("img/ear_neutral_left.png", -1)
+        right_ear_img = cv.imread("img/ear_neutral_right.png", -1)
+    return (left_ear_img, right_ear_img)
 
 def classify_emotion(frame_gray, face, model):
     """
     For some face input, normalized to 48x48, we will determine the emotion
+
+    Returns the softmax result of the output as an Emotion enum!
     """
     # slice out the face region
     (x, y, w, h) = face
@@ -114,7 +147,7 @@ def classify_emotion(frame_gray, face, model):
 
     # Predict & return classification
     prediction = model.predict(resized)
-    return int(np.argmax(prediction))
+    return Emotion(np.argmax(prediction))
 
 def apply_filter_to_frame(face, frame, emotion):
     """
